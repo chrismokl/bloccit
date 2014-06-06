f8499585bbb6283281be69e22d6eac48fc154d84
Chris Mo <cmo871@gmail.com>
2014-06-06 16:51:08 -0700

    associated and nested topics and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,20 @@ class PostsController < ApplicationController
   end
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
   def create
+    @topic = Topic.find(params[:topic:id])
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved"
@@ -26,11 +30,15 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
+    authorize @post
     if @post.update_attributes(params.require(:post).permit(:title,:body))
       flash[:notice] = "Post was updated."
       redirect_to @post
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,20 @@ require 'faker'
 end
 users = User.all
 
+# Create Topics
+15.times do
+  Topic.create(
+    name:  Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -30,10 +40,34 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes(
-  email: 'youremail.com',
-  password: 'helloworld'
+# Create an admin user
+admin = User.new(
+  name: 'Admin User',
+  email: 'cmo871@gmail.com',
+  password: 'hello',
+  role: 'admin'
 )
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'mod@example.com',
+  password: 'hello',
+  role: 'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+  name: 'Member User',
+  email: 'member@member.com',
+  password: 'hello'
+)
+member.skip_confirmation!
+member.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
