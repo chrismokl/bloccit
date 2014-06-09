5f5f2c33b24b050fd25dac83b0199420db24d46e
Chris Mo <cmo871@gmail.com>
2014-06-09 09:41:29 -0700

    configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # https://chrismo.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'chrismo.herokuapp.com' }
 end
